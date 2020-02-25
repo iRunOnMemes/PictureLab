3b64d81a3ace5d7cbb10abc6b51dfdd15ef2c1cf
iRunOnMemes <56176482+iRunOnMemes@users.noreply.github.com>
2020-02-25 15:02:52 -0800

    Look at notes
    Did a4 question 1

--- pixlab/IntArrayWorker.java
+++ pixlab/IntArrayWorker.java
@@ -11,6 +11,21 @@ public class IntArrayWorker
     matrix = theMatrix;
   }
   
+    /**
+   * a4.) Question 1
+   */
+  public int getCount(int n){
+     int count = 0;
+     for(int row = 0; row<matrix.length; row++)
+     {
+         for (int col = 0; col < matrix[0].length; col++){
+             if(matrix[row][col] ==n)
+             count++;
+            }
+         
+    }
+    return count;
+}
   /**
    * Method to return the total 
    * @return the total of the values in the array
